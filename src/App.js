95832e711f03b8fe9977c8c00368652a358ff1be
Sohel Rana <mdsrana11@gmail.com>
2023-11-04 11:59:40 +0600

    Added tasks and icon

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import {useState} from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+
+
+const App = () => {
+  const [tasks, setTasks] = useState ([
+    {
+        id: 1,
+        text: 'Doctor Appointment',
+        day: 'Feb 5th at 2:30pm',
+        remainder: true
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30pm',
+        remainder: true
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        remainder: false
+    }
+])
+  return(
+    <div className = 'container'>
+      <Header />
+      <Tasks tasks={tasks} /> 
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const header = ({ title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+    </header>
+  )
+}
+
+
+header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+header.prototype = {
+  title: PropTypes.string.isRequired,
+}
+
+export default header
