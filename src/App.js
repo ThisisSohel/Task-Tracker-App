54fb5df4a481c90284e4959605f19b7b2b67cbb2
Sohel Rana <mdsrana11@gmail.com>
2023-11-04 14:10:37 +0600

    Added someAddTask features to add new task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 const App = () => {
@@ -9,19 +10,19 @@ const App = () => {
         id: 1,
         text: 'Doctor Appointment',
         day: 'Feb 5th at 2:30pm',
-        remainder: true
+        reminder: true
     },
     {
         id: 2,
         text: 'Meeting at School',
         day: 'Feb 6th at 1:30pm',
-        remainder: true
+        reminder: true
     },
     {
         id: 3,
         text: 'Food Shopping',
         day: 'Feb 5th at 2:30pm',
-        remainder: false
+        reminder: false
     }
 ])
 
@@ -30,10 +31,16 @@ const deleteTask = (id) => {
    setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+
+const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, remainder: !task.remainder } : task))
+}
   return(
     <div className = 'container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/> 
+      <AddTask />
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>) : ('No Tasks to Show')}
     </div>
   )
 }
